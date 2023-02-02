ff6a1a1ea12bb51fcc2c8cf77f39e7182989f951
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-02-02 15:14:41 +0300

    ss

--- Assets/BTNCheck.cs
+++ Assets/BTNCheck.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class BTNCheck : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class BTNCheck : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler, IEndDragHandler
 {
 
     public bool buttonPressed;
     private string currentTag;
- 
+    private Vector2 _BTNtransform;
+    [SerializeField] Canvas canv;
+    private void Start()
+    {
+        
+        RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform)
+            canv.transform, transform.position, canv.worldCamera, out _BTNtransform);
+        //IM GONNA CRY!!
+        //it worked
+        //uses the transformutility, converts it to screentopointreactang
+        //makes use of canv.transform to get the bounds and transform
+        //transform.position for current position
+        //canv.worldcamera for changing the position coordinates
+       
+    }
     private void Update()
     {
       
@@ -35,4 +49,29 @@ public class BTNCheck : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         buttonPressed = false;
 
     }
+    public void OnDrag(PointerEventData data)
+    {
+        Vector2 position;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            (RectTransform)canv.transform, data.position,
+            canv.worldCamera, out position);
+        //moving elements in Unity
+        //requires canvas
+
+        
+      
+
+         if(position.y < _BTNtransform.y+20 && position.y > _BTNtransform.y -20 && position.x > _BTNtransform.x - 200)
+        {
+            transform.position = canv.transform.TransformPoint(position);
+        }
+
+        //this is the way you set position on canvas
+    }
+
+    public void OnEndDrag(PointerEventData data)
+    {
+        transform.position = canv.transform.TransformPoint(_BTNtransform);
+    }
+
 }
