2d2814647e729980d50da74e709b9f3bd5a080db
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-03-08 15:32:31 +0300

    NewChanges

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,18 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.PlayerLoop;
 
-public class Movement : MonoBehaviour
+public class Movement : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject Player;
+    private Rigidbody2D rb;
+    private float _runningSpeed = 3f;
+    private bool _btnRightPressed = false;
+    private bool _btnLeftPressed = false;
+    private SpriteRenderer sr;
+    private Animator anim;
+    private void Start()
     {
-        
+        Player = GameObject.FindWithTag("PlayerG") ? GameObject.FindWithTag("PlayerG") : GameObject.FindWithTag("PlayerB");
+        rb=Player.GetComponent<Rigidbody2D>();
+        sr=Player.GetComponent<SpriteRenderer>();
+        anim=Player.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        Player = GameObject.FindWithTag("PlayerG") ? GameObject.FindWithTag("PlayerG") : GameObject.FindWithTag("PlayerB");
+          
+           if(_btnRightPressed)
+           {
+
+                  Player.transform.Translate(_runningSpeed * Time.deltaTime, 0, 0);  
+                  sr.flipX = false;
+
+           }
+
+            if (_btnLeftPressed)
+            {
+                    Player.transform.Translate(-_runningSpeed * Time.deltaTime, 0, 0);
+                    sr.flipX = true;
+
+            }
+
+            if(_btnLeftPressed || _btnRightPressed)
+            {
+                 anim.SetBool("Run", true);
+            }
+           
+            if(anim.GetCurrentAnimatorStateInfo(0).IsName("Sliding"))
+            {
+                 if(sr.flipX)
+                 {
+                     rb.AddForce(-transform.right * 5f * Time.deltaTime, ForceMode2D.Impulse);
+                 }
+                  else
+                  {
+                     rb.AddForce(transform.right * 5f * Time.deltaTime, ForceMode2D.Impulse);
+
+                    }
+
+        }
         
+
+    }   
+
+    
+  
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (gameObject.name == "Right")
+        {
+
+
+            _btnRightPressed = true;
+         }
+        else
+        {
+           _btnLeftPressed= true;
+
+        }
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+        if (gameObject.name == "Right")
+        {
+
+
+            _btnRightPressed = false;
+
+        }
+        else
+        {
+            _btnLeftPressed = false;
+
+        }
+
+        anim.SetBool("Run", false);
+        anim.SetBool("sliding", true);
+
     }
 }
